ac53fd35ad6f9f4a2b378b6e628adcb6fc624ad0
mrAhmad <naashna20@gmail.com>
2021-07-20 19:12:05 +0430

    add variable to js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hello");
+let display = document.querySelector('#output-disp');
+let sign = document.querySelector('#output-sign');
+let stat = document.querySelector('#stat');
+const numbers = document.querySelectorAll('.btn');
+let backspace = document.querySelector('#backspace');
+let onOff = document.querySelector('#on-off');
+let equal = document.querySelector('#equal');
+let mp = document.querySelector('#mp');
+let point = document.querySelector('#point');
+let mul = document.querySelector('#mul');
+let div = document.querySelector('#div');
+let minut = document.querySelector('#minut');
+let plus = document.querySelector('#plus');
+
+
+onOff.addEventListener('click', ()=>{
+  display.textContent = '0';
+});
